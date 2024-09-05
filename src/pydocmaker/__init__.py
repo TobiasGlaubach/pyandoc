baf647f6ad3bf6db7abb6363c79f60c09b0f4955
Tobias Glaubach <tglaubach@mpifr-bonn.mpg.de>
2024-09-05 13:56:22 +0200

    updated the functions exposed to base level of the package to v1.3

--- src/pydocmaker/__init__.py
+++ src/pydocmaker/__init__.py
@@ -1,12 +1,7 @@
 __version__ = '1.3.0'
 
-from pydocmaker.core import DocBuilder, SectionedDoc, construct, constr, buildingblocks
+from pydocmaker.core import DocBuilder, construct, constr, buildingblocks
 
-from pydocmaker.exporters.ex_docx import convert as to_docx
-from pydocmaker.exporters.ex_html import convert as to_html
-from pydocmaker.exporters.ex_redmine import convert as to_redmine
-from pydocmaker.exporters.ex_redmine import convert as to_textile
-from pydocmaker.exporters.ex_tex import convert as to_tex
 
 def get_schema():
     return {k: getattr(constr, k)() for k in buildingblocks}
